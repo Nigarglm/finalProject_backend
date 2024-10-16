f7dbe8654d58c428644deab3ad2fb697fbd83942
Nigarglm <tu7mk3dbf@code.edu.az>
2024-10-16 16:39:03 +0400

    user model, controller and routes added

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import userRoutes from './routes/user.routes.js'
+import cors from 'cors'
+
+const app = express()
+
+dotenv.config()
+
+app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true,
+}));
+
+app.use('/users', userRoutes);
+
+
+const PORT = process.env.PORT
+const MONGODB_URL =process.env.MONGODB_URL
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on Port : ${PORT}`)
+})
+
+mongoose.connect(MONGODB_URL)
+.then(()=>{
+    console.log("Database connection established")
+})
+.catch((err)=>{
+    console.log(err);
+})
